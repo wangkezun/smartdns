ca25bd11e4df443d786d31c4bf9e5ec5b0ad9376
Tyrael <wangkezun@gmail.com>
2023-03-21 16:21:51 +0800

    update

--- script/converter.sh
+++ script/converter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl $1 |grep -v Premium|awk -F ',' '{gsub(/ /,"",$2)gsub(/ /,"",$3);printf "domain-rules /%s/ -speed-check-mode tcp:%s -nameserver domestic\n",$2,$3}'|sort|uniq > nexitally.conf
+curl $1 |grep -v Premium|awk -F ',' '{gsub(/ /,"",$2)gsub(/ /,"",$3);printf "domain-rules /%s/ -speed-check-mode tcp:%s -nameserver domestic\n",$2,$3}'|sort|uniq > $2
